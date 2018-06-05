db1ce01ef9fe05dce9706618c5d3b40f78379cfc
DoctorFoxy <31564892+DoctorFoxy@users.noreply.github.com>
2018-06-05 19:04:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,21 +13,33 @@ client.on('message', message => { //PING - PONG
 
 client.on('message', message => { //HELP MENU
     if (message.content === '-help') {
-        var helpmenu = "```\nThis is the help menu for MoonBot:\n-help: Shows this menu.\n-ping: Replies pong.\n-avatar: Gives you the link to your avatar.\n-techniques: Shows list of lucid dreaming techniques.\nUse -\"technique\" without the brackets to see an explanation of that tech. ```";
+        var helpmenu = "```\nThis is the help menu for MoonBot:\n-help: Shows this menu.\n-ping: Replies pong.\n-avatar: Gives you the link to your avatar.\n-techniques: Shows list of lucid dreaming techniques. Use -\"technique\" without the brackets to see an explanation of that tech. ```";
     	message.channel.send(helpmenu);
   	}   
 });
 
-client.on('message', message => {
+client.on('message', message => { // AVATAR COMMAND
   if (message.content === '-avatar') {
     message.reply(message.author.avatarURL);
   }
 });
 
-client.on('message', message => {
+client.on('message', message => { //TECHNIQUES COMMAND
   if (message.content === '-techniques') {
       var techs = "```wild, fild, mild, rc, wbtb, deild, dild```"
       message.channel.send(techs);
+  }
+});
+
+client.on('message', message => { // WILD
+  if (message.content === '-wild') {
+      message.channel.send("Wake Induced Lucid Dream: A technique where you maintain consciousness/awareness while your body falls asleep.");
+  }
+});
+
+client.on('message', message => { // FILD
+  if (message.content === '-fild') {
+      message.channel.send("Finger Induced Lucid Dream: Similar to a WILD but you use finger motions to preserve your conscious/awareness as you fall asleep.");
   }
 });
 
