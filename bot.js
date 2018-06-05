542e01e2c8ce61d02f2a320550dfbc8ded5673a0
DoctorFoxy <31564892+DoctorFoxy@users.noreply.github.com>
2018-06-05 18:36:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', () => { // BOT READY NOTIFICATION
     console.log('I am ready!');
 });
 
-client.on('message', message => {
+client.on('message', message => { //PING - PONG
     if (message.content === '-ping') {
     	message.reply('pong');
+  	}   
+});
+
+client.on('message', message => { //HELP MENU
+    if (message.content === '-help') {
+    	message.channel.send("test");
   	}   
 });
 
