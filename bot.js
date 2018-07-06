f807f20724ccb092a0f7ef6158e3d30fde909bdc
DoctorFoxy <31564892+DoctorFoxy@users.noreply.github.com>
2018-07-06 20:08:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./package.json');
 const reply = require('./messages.json');
+const urban = require('relevant-urban');
 
 client.on("ready", () => { // BOT READY NOTIFICATION
   console.log("I am ready!");
@@ -41,29 +42,19 @@ client.on("message", message => {
     }
 	
 	//ROCK PAPER SCISSORS:
-	var sendm = "message.channel.send"
-	var lose = sendm + "(\"You lost.\")", tie = sendm + "(\"Tie!\")", win = sendm + "(\"You win!\")",
-	rock = sendm + "(\"ROCK!\")", paper = sendm + "(\"PAPER!\")", scissors = sendm + "(\"SCISSORS!\")";
-	
 	if (com === "rps") {
-		random = Math.floor((Math.random() * 3) + 1);
-		if (args[1] == "rock" || args[1] == "r") { //ROCK
-			if (random == 1) { eval(rock); eval(tie); } 
-			if (random == 2) { eval(paper); eval(lose); } 
-			if (random == 3) { eval(scissors); eval(win); } 
-		}
-		else if (args[1] == "paper" || args[1] == "p") { //PAPER
-			if (random == 1) { eval(rock); eval(win); } 
-			if (random == 2) { eval(paper); eval(tie); } 
-			if (random == 3) { eval(scissors); eval(lose); } 
-		}
-		else if (args[1] == "scissors" || args[1] == "s") { //SCISSORS
-			if (random == 1) { eval(rock); eval(lose); } 
-			if (random == 2) { eval(paper); eval(win); } 
-			if (random == 3) { eval(scissors); eval(tie); } 
-		}
+		const rps = require('./commands/rps.js');
+		rps(args, message);
 	}
-	
+	//URBAN
+	else if (com === "urban") {
+		var testing = urban(args[1]);
+		testing.then((definition) => {
+		message.channel.send("``Word: " + definition.word + "``");
+		message.channel.send("``Definition: " + definition.definition + "``");
+		message.channel.send("``Example: " + definition.example + "``");
+	});
+	}
 	
     //NIGHTFORTUNE
     if (com === "fortune") {
