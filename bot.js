3ecd755e91dff47aa5d77916135b75a253f3d06e
DoctorFoxy <31564892+DoctorFoxy@users.noreply.github.com>
2020-05-08 12:13:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,8 @@ client.on("message", message => {
 	console.log("Pong");
 	    
 	const Embed = new Discord.RichEmbed()
-	.setDescription("Name: " + args[1] + ":\n" + "Attempts: " + args[(args.length-2)] + "\n" + "Success: " + args[(args.length-1)] + "\n" + "Score: " + (args[(args.length-2)]*1)*(args[(args.length-1)]*1+1) + "\n---")
+	.setTitle("Name: " + args[1])
+	.setDescription("Attempts: " + args[(args.length-2)] + "\n" + "Success: " + args[(args.length-1)] + "\n" + "Score: " + (args[(args.length-2)]*1)*(args[(args.length-1)]*1+1) + "\n---")
 	.setFooter(args[1], message.author.avatarURL);
 	message.channel.sendEmbed(Embed);
     }
