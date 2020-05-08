c8bb626f79f9e7e2f0127b1c795de0f2209071e6
DoctorFoxy <31564892+DoctorFoxy@users.noreply.github.com>
2020-05-08 12:04:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,20 +26,6 @@ client.on("message", message => {
 	const exampleEmbed = new discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 	
 	message.channel.send(exampleEmbed);
     }
